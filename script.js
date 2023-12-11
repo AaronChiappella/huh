cbea6991e0e0856a6bd615c61b517e2e37707453
AaronChiappella <aaronchiappella@gmail.com>
2023-12-10 23:35:05 -0300

    touch

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var imagen = document.getElementById('imagenPrincipal');
   var click = false;
 
-  document.addEventListener('mousedown', function() {
+  document.addEventListener('touchstart', function() {
     click = true;
     imagen.src = '/assets/boca-abierta.jpg';
-    
+    reproducirSonido('/assets/huh-sound.mp3');
   });
 
-  document.addEventListener('mouseup', function() {
+  document.addEventListener('touchend', function() {
     if (click) {
       click = false;
       imagen.src = '/assets/boca-cerrada.jpg';
-      
     }
   });
 
